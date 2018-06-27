6b5cd1d767f15e63179f9f237932e8abe86b250b
saswatipriyadarshini <saswati.p13@gmail.com>
2018-06-27 18:00:00 +0530

    1.Added realtime listener to database 2. Gave style to the user-feed

--- app.js
+++ app.js
@@ -4,33 +4,42 @@ const form = document.querySelector('#new-post-form');
 function renderPosts(doc){
 	let li = document.createElement('li');
 	let title = document.createElement('h3')
-	let name = document.createElement('h6');
+	let name = document.createElement('span');
 	let post = document.createElement('p');
-	let likeBtn = document.createElement('button');
 	let deleteBtn = document.createElement('button');
 
 	li.setAttribute('data-id', doc.id);
 	title.textContent = doc.data().title;
 	name.textContent  = doc.data().name;
 	post.textContent  = doc.data().post;
-	likeBtn.textContent = 'Like';
 	deleteBtn.textContent = 'Delete';
+
+	title.id = 'title';
+	name.id = 'user-name';
+	post.id = 'user-post';
+	deleteBtn.id = 'deleteBtn'
 
 	li.appendChild(title)
 	li.appendChild(name);
 	li.appendChild(post);
-	li.appendChild(likeBtn);
 	li.appendChild(deleteBtn);
 
 	userPost.appendChild(li);
 
+	//deleting data
+	deleteBtn.addEventListener('click', (e) => {
+		e.stopPropagation();
+		let id = e.target.parentElement.getAttribute('data-id');
+		db.collection('posts').doc(id).delete();
+	});
+
 }
 //getting data from the datasbase
-db.collection('posts').get().then((snapshot) => {
-	snapshot.docs.forEach(doc => {
-		renderPosts(doc);
-	});
-});
+// db.collection('posts').get().then((snapshot) => {
+// 	snapshot.docs.forEach(doc => {
+// 		renderPosts(doc);
+// 	});
+// });
 
 //saving data
 form.addEventListener('submit', (e) => {
@@ -46,3 +55,18 @@ form.addEventListener('submit', (e) => {
 
 	$('#add-post-modal').hide();
 });	
+
+//realtime listener
+db.collection('posts').orderBy('name').onSnapshot(snapshot => {
+	let changes = snapshot.docChanges();
+	changes.forEach(change => {
+		console.log(change.doc.data());
+		if(change.type == 'added'){
+			renderPosts(change.doc);
+		} else if (change.type == 'removed'){
+			let li = userPost.querySelector('[data-id=' + change.doc.id + ']');
+			userPost.removeChild(li);
+		}
+	})
+	
+});
